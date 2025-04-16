d3c4255876f0feef2c0253160f94f4d77d1b38f0
TenTakano <takano20771@gmail.com>
2025-04-16 20:49:50 +0900

    [chore] Create Project

---
+++ lib/mold.ex
@@ -0,0 +1,18 @@
+defmodule Mold do
+  @moduledoc """
+  Documentation for `Mold`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Mold.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/mold_test.exs
@@ -0,0 +1,8 @@
+defmodule MoldTest do
+  use ExUnit.Case
+  doctest Mold
+
+  test "greets the world" do
+    assert Mold.hello() == :world
+  end
+end
