48bcfdbc153a720c31af95e9d8501394bfe11603
TenTakano <takano20771@gmail.com>
2025-04-19 03:17:06 +0900

    [refactor] Fix Test Assertion Format

--- test/mold/builder_test.exs
+++ test/mold/builder_test.exs
@@ -32,8 +32,8 @@ defmodule Mold.BuilderTest do
 
       params = %{name: "John Doe"}
 
-      assert {:error, errors} = Builder.build(schema, params)
-      assert errors == [age: "Missing required key"]
+      assert Builder.build(schema, params) ==
+               {:error, [age: "Missing required key"]}
     end
 
     test "returns error for invalid values" do
@@ -43,8 +43,8 @@ defmodule Mold.BuilderTest do
 
       params = %{age: "not an integer"}
 
-      assert {:error, errors} = Builder.build(schema, params)
-      assert errors == [age: "Invalid integer value: \"not an integer\""]
+      assert Builder.build(schema, params) ==
+               {:error, [age: "Invalid integer value: \"not an integer\""]}
     end
 
     test "returns multiple errors" do
@@ -55,152 +55,197 @@ defmodule Mold.BuilderTest do
 
       params = %{name: nil, age: "not an integer"}
 
-      assert {:error, errors} = Builder.build(schema, params)
-
-      assert errors == [
-               name: "Missing required key",
-               age: "Invalid integer value: \"not an integer\""
-             ]
+      assert Builder.build(schema, params) ==
+               {:error,
+                [
+                  name: "Missing required key",
+                  age: "Invalid integer value: \"not an integer\""
+                ]}
     end
   end
 
   describe "get_value/3" do
     test "returns value of existing key" do
-      assert {:ok, "value"} = Builder.get_value(%{key: "value"}, :key, required: true)
-      assert {:ok, "value"} = Builder.get_value(%{key: "value"}, :key, required: false)
+      assert Builder.get_value(%{key: "value"}, :key, required: true) ==
+               {:ok, "value"}
+
+      assert Builder.get_value(%{key: "value"}, :key, required: false) ==
+               {:ok, "value"}
     end
 
     test "returns default value for missing key" do
-      assert {:ok, "default"} = Builder.get_value(%{}, :key, default: "default")
+      assert Builder.get_value(%{}, :key, default: "default") ==
+               {:ok, "default"}
     end
 
     test "returns nil for missing key with no default if not required" do
-      assert {:ok, nil} = Builder.get_value(%{}, :key, required: false)
+      assert Builder.get_value(%{}, :key, required: false) ==
+               {:ok, nil}
     end
 
     test "returns error for missing required key" do
-      assert {:error, "Missing required key"} = Builder.get_value(%{}, :key, required: true)
+      assert Builder.get_value(%{}, :key, required: true) ==
+               {:error, "Missing required key"}
     end
 
     test "returns error for nil value if required" do
-      assert {:error, "Missing required key"} =
-               Builder.get_value(%{key: nil}, :key, required: true)
+      assert Builder.get_value(%{key: nil}, :key, required: true) ==
+               {:error, "Missing required key"}
     end
   end
 
   describe "build_string/2" do
     test "converts value to string" do
-      assert {:ok, "test"} = Builder.build_string("test", [])
-      assert {:ok, "123"} = Builder.build_string(123, [])
-      assert {:ok, "123.45"} = Builder.build_string(123.45, [])
+      assert Builder.build_string("test", []) == {:ok, "test"}
+      assert Builder.build_string(123, []) == {:ok, "123"}
+      assert Builder.build_string(123.45, []) == {:ok, "123.45"}
     end
 
     test "returns error for invalid value" do
-      assert {:error, "Invalid string value: nil"} = Builder.build_string(nil, [])
-      assert {:error, "Invalid string value: []"} = Builder.build_string([], [])
+      assert Builder.build_string(nil, []) ==
+               {:error, "Invalid string value: nil"}
+
+      assert Builder.build_string([], []) ==
+               {:error, "Invalid string value: []"}
     end
   end
 
   describe "build_integer/2" do
     test "converts value to integer" do
-      assert {:ok, 123} = Builder.build_integer(123, [])
-      assert {:ok, 123} = Builder.build_integer("123", [])
+      assert Builder.build_integer(123, []) == {:ok, 123}
+      assert Builder.build_integer("123", []) == {:ok, 123}
     end
 
     test "returns error for invalid value" do
-      assert {:error, "Invalid integer value: 123.45"} = Builder.build_integer(123.45, [])
-      assert {:error, "Invalid integer value: nil"} = Builder.build_integer(nil, [])
-      assert {:error, "Invalid integer value: []"} = Builder.build_integer([], [])
-      assert {:error, "Invalid integer value: \"abc\""} = Builder.build_integer("abc", [])
+      assert Builder.build_integer(123.45, []) ==
+               {:error, "Invalid integer value: 123.45"}
+
+      assert Builder.build_integer(nil, []) ==
+               {:error, "Invalid integer value: nil"}
+
+      assert Builder.build_integer([], []) ==
+               {:error, "Invalid integer value: []"}
+
+      assert Builder.build_integer("abc", []) ==
+               {:error, "Invalid integer value: \"abc\""}
     end
   end
 
   describe "build_float/2" do
     test "converts value to float" do
-      assert {:ok, 123.45} = Builder.build_float(123.45, [])
-      assert {:ok, 123.45} = Builder.build_float("123.45", [])
+      assert Builder.build_float(123.45, []) == {:ok, 123.45}
+      assert Builder.build_float("123.45", []) == {:ok, 123.45}
     end
 
     test "returns error for invalid value" do
-      assert {:error, "Invalid float value: 123"} = Builder.build_float(123, [])
-      assert {:error, "Invalid float value: nil"} = Builder.build_float(nil, [])
-      assert {:error, "Invalid float value: []"} = Builder.build_float([], [])
-      assert {:error, "Invalid float value: \"abc\""} = Builder.build_float("abc", [])
+      assert Builder.build_float(123, []) ==
+               {:error, "Invalid float value: 123"}
+
+      assert Builder.build_float(nil, []) ==
+               {:error, "Invalid float value: nil"}
+
+      assert Builder.build_float([], []) ==
+               {:error, "Invalid float value: []"}
+
+      assert Builder.build_float("abc", []) ==
+               {:error, "Invalid float value: \"abc\""}
     end
   end
 
   describe "build_boolean/2" do
     test "converts value to boolean" do
-      assert {:ok, true} = Builder.build_boolean(true, [])
-      assert {:ok, false} = Builder.build_boolean(false, [])
-      assert {:ok, true} = Builder.build_boolean("true", [])
-      assert {:ok, false} = Builder.build_boolean("false", [])
+      assert Builder.build_boolean(true, []) == {:ok, true}
+      assert Builder.build_boolean(false, []) == {:ok, false}
+      assert Builder.build_boolean("true", []) == {:ok, true}
+      assert Builder.build_boolean("false", []) == {:ok, false}
     end
 
     test "returns error for invalid value" do
-      assert {:error, "Invalid boolean value: nil"} = Builder.build_boolean(nil, [])
-      assert {:error, "Invalid boolean value: []"} = Builder.build_boolean([], [])
-      assert {:error, "Invalid boolean value: \"abc\""} = Builder.build_boolean("abc", [])
+      assert Builder.build_boolean(nil, []) ==
+               {:error, "Invalid boolean value: nil"}
+
+      assert Builder.build_boolean([], []) ==
+               {:error, "Invalid boolean value: []"}
+
+      assert Builder.build_boolean("abc", []) ==
+               {:error, "Invalid boolean value: \"abc\""}
     end
   end
 
   describe "build_atom/2" do
     test "returns the atom value" do
-      assert {:ok, :test} = Builder.build_atom(:test, [])
+      assert Builder.build_atom(:test, []) == {:ok, :test}
     end
 
     test "returns error for non-atom value" do
-      assert {:error, "Invalid atom value: []"} = Builder.build_atom([], [])
-      assert {:error, "Invalid atom value: \"abc\""} = Builder.build_atom("abc", [])
+      assert Builder.build_atom([], []) ==
+               {:error, "Invalid atom value: []"}
+
+      assert Builder.build_atom("abc", []) ==
+               {:error, "Invalid atom value: \"abc\""}
     end
   end
 
   describe "build_time/2" do
     test "returns the time value" do
-      assert {:ok, ~T[12:34:56]} = Builder.build_time(~T[12:34:56], [])
-      assert {:ok, ~T[12:34:56]} = Builder.build_time("12:34:56", [])
-      assert {:ok, ~T[12:34:56]} = Builder.build_time(~U[2023-10-01 12:34:56Z], [])
+      assert Builder.build_time(~T[12:34:56], []) ==
+               {:ok, ~T[12:34:56]}
+
+      assert Builder.build_time("12:34:56", []) ==
+               {:ok, ~T[12:34:56]}
+
+      assert Builder.build_time(~U[2023-10-01 12:34:56Z], []) ==
+               {:ok, ~T[12:34:56]}
     end
 
     test "returns error for non-time value" do
-      assert {:error, "Given value is not ISO8601 format: \"45:67:89\""} =
-               Builder.build_time("45:67:89", [])
+      assert Builder.build_time("45:67:89", []) ==
+               {:error, "Given value is not ISO8601 format: \"45:67:89\""}
 
-      assert {:error, "Invalid time value: []"} = Builder.build_time([], [])
+      assert Builder.build_time([], []) ==
+               {:error, "Invalid time value: []"}
     end
   end
 
   describe "build_date/2" do
     test "returns the date value" do
-      assert {:ok, ~D[2023-10-01]} = Builder.build_date(~D[2023-10-01], [])
-      assert {:ok, ~D[2023-10-01]} = Builder.build_date("2023-10-01", [])
-      assert {:ok, ~D[2023-10-01]} = Builder.build_date(~U[2023-10-01 12:34:56Z], [])
+      assert Builder.build_date(~D[2023-10-01], []) ==
+               {:ok, ~D[2023-10-01]}
+
+      assert Builder.build_date("2023-10-01", []) ==
+               {:ok, ~D[2023-10-01]}
+
+      assert Builder.build_date(~U[2023-10-01 12:34:56Z], []) ==
+               {:ok, ~D[2023-10-01]}
     end
 
     test "returns error for non-date value" do
-      assert {:error, "Given value is not ISO8601 format: \"2023-10-32\""} =
-               Builder.build_date("2023-10-32", [])
+      assert Builder.build_date("2023-10-32", []) ==
+               {:error, "Given value is not ISO8601 format: \"2023-10-32\""}
 
-      assert {:error, "Invalid date value: []"} = Builder.build_date([], [])
+      assert Builder.build_date([], []) ==
+               {:error, "Invalid date value: []"}
     end
   end
 
   describe "build_datetime/2" do
     test "returns the datetime value" do
-      assert {:ok, ~U[2023-10-01 12:34:56Z]} =
-               Builder.build_datetime(~U[2023-10-01 12:34:56Z], [])
+      assert Builder.build_datetime(~U[2023-10-01 12:34:56Z], []) ==
+               {:ok, ~U[2023-10-01 12:34:56Z]}
 
-      assert {:ok, ~U[2023-10-01 12:34:56Z]} = Builder.build_datetime("2023-10-01T12:34:56Z", [])
+      assert Builder.build_datetime("2023-10-01T12:34:56Z", []) ==
+               {:ok, ~U[2023-10-01 12:34:56Z]}
 
-      assert {:ok, ~U[2023-10-01 01:00:00Z]} =
-               Builder.build_datetime("2023-10-01T10:00:00+09:00", [])
+      assert Builder.build_datetime("2023-10-01T10:00:00+09:00", []) ==
+               {:ok, ~U[2023-10-01 01:00:00Z]}
     end
 
     test "returns error for non-datetime value" do
-      assert {:error, "Given value is not ISO8601 format: \"2023-10-01T12:34:56\""} =
-               Builder.build_datetime("2023-10-01T12:34:56", [])
+      assert Builder.build_datetime("2023-10-01T12:34:56", []) ==
+               {:error, "Given value is not ISO8601 format: \"2023-10-01T12:34:56\""}
 
-      assert {:error, "Invalid datetime value: []"} = Builder.build_datetime([], [])
+      assert Builder.build_datetime([], []) ==
+               {:error, "Invalid datetime value: []"}
     end
   end
 end
